acbabeb97d6dada642108a7b20677f2c63934e5a
ARNAV ASHOK BAVARVA <arnavbavarva@icloud.com>
2025-07-28 15:14:42 +0530

    Initial Commit

---
+++ Synkr/SynkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynkrApp.swift
+//  Synkr
+//
+//  Created by Arnav on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SynkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
